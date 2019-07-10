c59d2e5ab033f10d2de5f81e1d843c663a892940
jh-yang <ofc.gracia@gmail.com>
2019-07-10 15:52:45 +0900

    to read trace file in miasm environment

---
+++ my/trace.py
@@ -0,0 +1,146 @@
+from __future__ import print_function
+from miasm.arch.x86.arch import mn_x86
+from miasm.arch.x86.regs import EDX
+from miasm.core.locationdb import LocationDB
+from miasm.analysis.binary import Container
+from miasm.analysis.machine import Machine
+from pdb import pm
+from miasm.core import parse_asm    
+from future.utils import viewitems
+from miasm.arch.x86.regs import EDX
+from typing import NamedTuple
+
+class Asm_instruction:
+    ta = int()
+	pa = ""
+	name = ""
+    operand = []
+	
+    def set(self,ta,pa,name, operand):
+		self.ta = ta
+		self.pa = pa
+        self.name = name
+        self.operand = operand
+
+    def get_ta(self):
+        return self.ta
+
+    def get_pa(self):
+        return self.pa
+
+    def get_name(self):
+        return self.name
+
+    def get_operand(self):
+        return self.operand
+
+    def from_string(self,ta,pa,string):
+        li = string.split(' ')
+        for i in range(len(li)):
+            li[i].strip()
+        opc = li[0]
+        del li[0]
+        self.set(int(ta),pa,opc,li)
+
+def is_next_varying(ta_list, trace_list):
+	if isinstance(ta_list,list):
+		cmp_pa = trace_list[ta_list[0]]
+		for ta in ta_list:
+			pa = trace_list[ta]
+			if cmp_pa != pa:
+				raise ValueError
+		return True
+	else:
+		return True
+
+def non_varying_predicate_pa(ta_map,trace_list):
+	keys = list(ta_map.keys())
+	pa_list = []
+	for key in keys:
+		ta_list = ta_map[key]
+		if is_next_varying(ta_list,trace_list):
+			pa_list.append(key)
+	
+	return pa_list
+		
+
+def cmp_jz_next_ta(ta, trace_list):
+	if(trace_list[ta].get_name() == 'cmp') and (trace_list[ta+1].get_name() == 'jz'):
+		return ta+1
+	else:
+		raise ValueError
+		
+def cmp_jz_next_pa(ta, trace_list):
+	ta2 = cmp_jz_next_ta(ta,trace_list)
+	return trace_list[ta2].get_pa()
+
+def group_cmp_jz(cmp_jz_ta_group,trace_list):
+	ta_map = dict()
+	for i in range(0,cmp_jz_ta_group):
+		pa = trace_list[cmp_jz_ta_group[i]].get_pa()
+		if pa in ta_map:
+			if isinstance(ta_map[pa],list):
+				li = ta_map[pa]
+				li.append(cmp_jz_ta_group[i])
+				ta_map[pa] = li
+			else:
+				li = []
+				li.append(ta_map[pa])
+				li.append(cmp_jz_ta_group[i])
+				ta_map[pa] = li
+		else:
+			ta_map[pa] = cmp_jz_ta_group[i]
+    
+	return ta_map
+
+def cmp_jz(trace_list):
+    cmp_jz_ta = []
+    for i in range(0,len(trace_list)):
+        if (trace_list[i].get_name() == 'cmp') :
+			if(trace_list[i+1].get_name() == 'jz'):
+				cmp_jz_ta.append(i)
+    return cmp_jz_ta_group
+
+def trace_from_file(filename):
+    f= open(filename, "rw")
+    lines = f.readlines()
+	binstr = "" # binary code
+	trace_list = []
+    for line in lines:
+		asm_instr = Asm_instruction()
+        li = line.split(";")
+		asm_instr.from_string(li[0].strip(),li[1].strip(),li[3].strip())
+		trace_list.append(asm_instr)
+        li2 = li[2].split('\\x')
+        for b in li2:
+            binstr += b.strip()
+    binstr = bytearray.fromhex(binstr)
+    f.close()
+
+    return str(binstr), trace_list
+
+def main():
+    binstr,trace_list = trace_from_file('calltrace.log')
+    c = Container.from_string(binstr)
+    machine = Machine('x86_64')
+    mdis = machine.dis_engine(c.bin_stream)
+    asmcfg = mdis.dis_multiblock(0)
+    
+    for block in asmcfg.blocks:
+        #block.lines[0].args[0] = EDX
+        #print(block.lines[0])
+        #print(block.lines[0].name)
+        print(block.to_string(asmcfg.loc_db))
+    #print (asmcfg.loc_db._offset_to_loc_key)
+
+    loc_db = LocationDB()
+    ira = machine.ira(loc_db)
+    ircfg = ira.new_ircfg_from_asmcfg(asmcfg)
+    
+    for lbl, irblock in viewitems(ircfg.blocks):
+        print(irblock)
+
+#    asmcfg = parse_asm.parse_txt(mn_x86, 64,binstr)
+#    loc_db.set_location_offset(loc_db.get_name_location("main"), 0x0)
+
+main()
