c4c55bd407edbe2acb5bcac98e184b5abfc5b1dc
jh-yang <ofc.gracia@gmail.com>
2019-07-11 01:27:06 -0700

    (19.07.11)

--- my/trace.py
+++ my/trace.py
@@ -1,14 +1,17 @@
 from __future__ import print_function
 from miasm.arch.x86.arch import mn_x86
-from miasm.arch.x86.regs import EDX
+#from miasm.arch.x86.regs import EDX
 from miasm.core.locationdb import LocationDB
 from miasm.analysis.binary import Container
 from miasm.analysis.machine import Machine
+
+from miasm.ir.symbexec import SymbolicExecutionEngine
+from miasm.expression.expression import *
+
 from pdb import pm
 from miasm.core import parse_asm    
 from future.utils import viewitems
-from miasm.arch.x86.regs import EDX
-from typing import NamedTuple
+
 
 class Asm_instruction:
     ta = int()
@@ -17,8 +20,8 @@ class Asm_instruction:
     operand = []
 	
     def set(self,ta,pa,name, operand):
-		self.ta = ta
-		self.pa = pa
+	self.ta = ta
+	self.pa = pa
         self.name = name
         self.operand = operand
 
@@ -67,79 +70,78 @@ def is_zf_zero(ta, trace_list, binstr):
 
 def additional_constraint_str(ta,trace_list):
     if is_zf_zer(ta,trace_list):
-	print('zf == 1')
+        print('zf == 1')
     else:
-	print('zf == 0')
+        print('zf == 0')
 
 def is_next_varying(ta_list, trace_list):
-	if isinstance(ta_list,list):
-		cmp_pa = trace_list[ta_list[0]]
-		for ta in ta_list:
-			pa = trace_list[ta]
-			if cmp_pa != pa:
-				raise ValueError
-		return True
+    if isinstance(ta_list,list):
+        cmp_pa = trace_list[ta_list[0]]
+	for ta in ta_list:
+	    pa = trace_list[ta]
+            if cmp_pa != pa:
+		raise ValueError
+	    return True
 	else:
-		return True
+            return True
 
 def non_varying_predicate_pa(ta_map,trace_list):
-	keys = list(ta_map.keys())
-	pa_list = []
-	for key in keys:
-		ta_list = ta_map[key]
-		if is_next_varying(ta_list,trace_list):
-			pa_list.append(key)
+    keys = list(ta_map.keys())
+    pa_list = []
+    for key in keys:
+    	ta_list = ta_map[key]
+    	if is_next_varying(ta_list,trace_list):
+            pa_list.append(key)
 	
-	return pa_list
+    return pa_list
 		
 
 def cmp_jz_next_ta(ta, trace_list):
-	if(trace_list[ta].get_name() == 'cmp') and (trace_list[ta+1].get_name() == 'jz'):
-		return ta+1
-	else:
-		raise ValueError
+    if(trace_list[ta].get_name() == 'cmp') and (trace_list[ta+1].get_name() == 'jz'):
+        return ta+1
+    else:
+    	raise ValueError
 		
 def cmp_jz_next_pa(ta, trace_list):
-	ta2 = cmp_jz_next_ta(ta,trace_list)
-	return trace_list[ta2].get_pa()
+    ta2 = cmp_jz_next_ta(ta,trace_list)
+    return trace_list[ta2].get_pa()
 
 def group_cmp_jz(cmp_jz_ta_group,trace_list):
-	ta_map = dict()
-	for i in range(0,cmp_jz_ta_group):
-		pa = trace_list[cmp_jz_ta_group[i]].get_pa()
-		if pa in ta_map:
-			if isinstance(ta_map[pa],list):
-				li = ta_map[pa]
-				li.append(cmp_jz_ta_group[i])
-				ta_map[pa] = li
-			else:
-				li = []
-				li.append(ta_map[pa])
-				li.append(cmp_jz_ta_group[i])
-				ta_map[pa] = li
-		else:
-			ta_map[pa] = cmp_jz_ta_group[i]
-    
-	return ta_map
+    ta_map = dict()
+    for i in range(0,cmp_jz_ta_group):
+	pa = trace_list[cmp_jz_ta_group[i]].get_pa()
+	if pa in ta_map:
+            if isinstance(ta_map[pa],list):
+	        li = ta_map[pa]
+		li.append(cmp_jz_ta_group[i])
+		ta_map[pa] = li
+	    else:
+		li = []
+		li.append(ta_map[pa])
+	        li.append(cmp_jz_ta_group[i])
+		ta_map[pa] = li
+	else:
+	    ta_map[pa] = cmp_jz_ta_group[i]
+    return ta_map
 
 def cmp_jz(trace_list):
     cmp_jz_ta = []
     for i in range(0,len(trace_list)):
         if (trace_list[i].get_name() == 'cmp') :
-			if(trace_list[i+1].get_name() == 'jz'):
-				cmp_jz_ta.append(i)
+            if(trace_list[i+1].get_name() == 'jz'):
+		cmp_jz_ta.append(i)
     return cmp_jz_ta_group
 
 def trace_from_file(filename):
     f= open(filename, "rw")
     lines = f.readlines()
-	binstr = "" # binary code
-	trace_list = []
+    binstr = "" # binary code
+    trace_list = []
     for line in lines:
-		asm_instr = Asm_instruction()
+	asm_instr = Asm_instruction()
         li = line.split(";")
-		asm_instr.from_string(li[0].strip(),li[1].strip(),li[3].strip())
-		trace_list.append(asm_instr)
+	asm_instr.from_string(li[0].strip(),li[1].strip(),li[3].strip())
+	trace_list.append(asm_instr)
         li2 = li[2].split('\\x')
         for b in li2:
             binstr += b.strip()
@@ -150,25 +152,59 @@ def trace_from_file(filename):
 
 def main():
     binstr,trace_list = trace_from_file('calltrace.log')
+    loc_db = LocationDB()
+    ira = machine.ira(loc_db)
     c = Container.from_string(binstr)
     machine = Machine('x86_64')
     mdis = machine.dis_engine(c.bin_stream)
-    asmcfg = mdis.dis_multiblock(0)
-    
-    for block in asmcfg.blocks:
-        #block.lines[0].args[0] = EDX
-        #print(block.lines[0])
-        #print(block.lines[0].name)
-        print(block.to_string(asmcfg.loc_db))
-    #print (asmcfg.loc_db._offset_to_loc_key)
 
-    loc_db = LocationDB()
-    ira = machine.ira(loc_db)
-    ircfg = ira.new_ircfg_from_asmcfg(asmcfg)
+    ############################################
+#    asmcfg = mdis.dis_multiblock(0)
     
-    for lbl, irblock in viewitems(ircfg.blocks):
-        print(irblock)
+#    for block in asmcfg.blocks:
+#        #block.lines[0].args[0] = EDX
+#        #print(block.lines[0])
+#        #print(block.lines[0].name)
+#        print(block.to_string(asmcfg.loc_db))
+#    #print (asmcfg.loc_db._offset_to_loc_key)
+    
+    #############################################    
 
+    #++++++++++++++++++++++++++++++++++++++++++++#
+    offset = 0
+    sym_state = None
+    i = 0
+    while offset < len(binstr):
+        if i>10:
+            break
+        asmblock = mdis.dis_block(offset)
+        start, end = asmblock.get_range()
+        
+        ira = machine.ira(mdis.loc_db)
+        ircfg = ira.new_ircfg()
+        ira.add_asmblock_to_ircfg(asmblock, ircfg)
+
+        symb = SymbolicExecutionEngine(ira)
+        cur_addr = symb.run_at(ircfg,offset)
+
+        for state in symb.get_state():
+            print(state)
+        if sym_state is None:
+            sym_state = symb.get_state()
+        else:
+            syb_state.merge(symb.get_state())
+        print(offset)
+        offet = end
+        i += 1
+    for state in sym_state:
+        print(state)
+
+    #++++++++++++++++++++++++++++++++++++++++++++#
+#    ircfg = ira.new_ircfg_from_asmcfg(asmcfg)
+    
+#    for lbl, irblock in viewitems(ircfg.blocks):
+#        print(irblock)
+    #################################################
 #    asmcfg = parse_asm.parse_txt(mn_x86, 64,binstr)
 #    loc_db.set_location_offset(loc_db.get_name_location("main"), 0x0)
 
