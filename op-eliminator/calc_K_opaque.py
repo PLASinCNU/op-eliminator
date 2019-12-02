e0336e41bbdf909bbc471a360cb356e7843cbf65
jh-yang <ofc.gracia@gmail.com>
2019-12-02 15:58:29 +0900

    Update calc_K_opaque.py
    modified

--- op-eliminator/calc_K_opaque.py
+++ op-eliminator/calc_K_opaque.py
@@ -29,6 +29,32 @@ CF_JUMPS = ["JB", "JAE", "JBE", "JA"]  # Arith, Binary
 #PF_JUMPS = ["JPE", "JNP"]  # Arith
 CX_JUMPS = ["JCXZ", "JECXZ", "JRCXZ"]  # ECX
 
+# REGISTER_CONVERT_DICT = dict()
+ACCUMULATOR_REGISTERS = ["RAX", "EAX", "AX", "AL", "AH"]
+BASE_REGISTERS = ["RBX", "EBX", "BX", "BL", "BH"]
+COUNTER_REGISTERS = ["RCX", "ECX", "CX", "CL", "CH"]
+DATA_REGISTERS = ["RDX", "EDX", "DX", "DL", "DH"]
+STACK_POINTER_REGISTERS = ["RSP", "ESP", "SP"]
+STACK_BASE_POINTER_REGISTERS = ["RBP", "EBP", "BP"]
+SOURCE_REGISTERS = ["RSI", "ESI", "SI"]
+DESTINATION_REGISTERS = ["RDI", "EDI", "DI"]
+# for reg in ACCUMULATOR_REGISTERS:
+#     REGISTER_CONVERT_DICT[reg] = "EAX"
+# for reg in BASE_REGISTERS:
+#     REGISTER_CONVERT_DICT[reg] = "EBX"
+# for reg in COUNTER_REGISTERS:
+#     REGISTER_CONVERT_DICT[reg] = "ECX"
+# for reg in DATA_REGISTERS:
+#     REGISTER_CONVERT_DICT[reg] = "EDX"
+# for reg in STACK_POINTER_REGISTERS:
+#     REGISTER_CONVERT_DICT[reg] = "ESP"
+# for reg in STACK_BASE_POINTER_REGISTERS:
+#     REGISTER_CONVERT_DICT[reg] = "EBP"
+# for reg in SOURCE_REGISTERS:
+#     REGISTER_CONVERT_DICT[reg] = "ESI"
+# for reg in DESTINATION_REGISTERS:
+#     REGISTER_CONVERT_DICT[reg] = "EDI"
+
 
 class AsmInstruction:
     ta = int()
@@ -132,14 +158,37 @@ def BinToStr(trace_list, k, cjmp_ta):
     return bytearray.fromhex(binstr)
 
 
+def RegisterTranslator(reg):
+    if reg in ACCUMULATOR_REGISTERS:
+        unified_reg = "EAX"
+    elif reg in BASE_REGISTERS:
+        unified_reg = "EBX"
+    elif reg in COUNTER_REGISTERS:
+        unified_reg = "ECX"
+    elif reg in DATA_REGISTERS:
+        unified_reg = "EDX"
+    elif reg in STACK_POINTER_REGISTERS:
+        unified_reg = "ESP"
+    elif reg in STACK_BASE_POINTER_REGISTERS:
+        unified_reg = "EBP"
+    elif reg in SOURCE_REGISTERS:
+        unified_reg = "ESI"
+    elif reg in DESTINATION_REGISTERS:
+        unified_reg = "EDX"
+    else:
+        unified_reg = reg
+
+    return unified_reg
+
+
 def OF_Factor(trace_list, cjmp_ta):
     factors = []
     ta = cjmp_ta-1
-    while True:
+    while ta >= cjmp_ta-10 and ta > 0:
         if trace_list[ta].get_name().upper() in ARITHMETIC_OPERATORS + COMPARE_OPERATORS:
             for reg in trace_list[ta].get_operand():
                 if not reg.startswith("0x"):
-                    factors.append(reg.upper())
+                    factors.append(RegisterTranslator(reg.upper()))
             break
         ta -= 1
     return factors, ta
@@ -148,11 +197,11 @@ def OF_Factor(trace_list, cjmp_ta):
 def SF_Factor(trace_list, cjmp_ta):
     factors = []
     ta = cjmp_ta - 1
-    while True:
+    while ta >= cjmp_ta-10 and ta > 0:
         if trace_list[ta].get_name().upper() in ARITHMETIC_OPERATORS + LOGICAL_OPERATORS + COMPARE_OPERATORS:
             for reg in trace_list[ta].get_operand():
                 if not reg.startswith("0x"):
-                    factors.append(reg.upper())
+                    factors.append(RegisterTranslator(reg.upper()))
             break
         ta -= 1
     return factors, ta
@@ -161,11 +210,11 @@ def SF_Factor(trace_list, cjmp_ta):
 def CF_Factor(trace_list, cjmp_ta):
     factors = []
     ta = cjmp_ta - 1
-    while True:
+    while ta >= cjmp_ta-10 and ta > 0:
         if trace_list[ta].get_name().upper() in ARITHMETIC_OPERATORS + BINARY_OPERATORS + COMPARE_OPERATORS:
             for reg in trace_list[ta].get_operand():
                 if not reg.startswith("0x"):
-                    factors.append(reg.upper())
+                    factors.append(RegisterTranslator(reg.upper()))
             break
         ta -= 1
     return factors, ta
@@ -174,15 +223,32 @@ def CF_Factor(trace_list, cjmp_ta):
 def ZF_Factor(trace_list, cjmp_ta):
     factors = []
     ta = cjmp_ta - 1
-    while True:
-        #print(trace_list[ta].get_name().upper())
+    while ta >= cjmp_ta-10 and ta > 0:
         if trace_list[ta].get_name().upper() in ARITHMETIC_OPERATORS + LOGICAL_OPERATORS + COMPARE_OPERATORS:
             for reg in trace_list[ta].get_operand():
                 if not reg.startswith("0x"):
-                    factors.append(reg.upper())
+                    factors.append(RegisterTranslator(reg.upper()))
             break
         ta -= 1
     factors = list(set(factors))
+    return factors, ta
+
+
+def CX_Factor(trace_list, cjmp_ta):
+    factors = ["ECX"]
+    ta = cjmp_ta - 1
+    while ta >= cjmp_ta-10 and ta > 0:
+        operand = trace_list[ta].get_operand()
+
+        if RegisterTranslator(operand[0]) in COUNTER_REGISTERS:
+            if trace_list[ta].get_name().upper() in ARITHMETIC_OPERATORS:
+                if not operand[1].startswith("0x"):
+                    factors.append(RegisterTranslator(operand[1]))
+                break
+            elif trace_list[ta].get_name().upper() in DATA_MOVEMENT:
+                break
+        ta -= 1
+
     return factors, ta
 #
 # def PF_Factor(trace_list, cjmp_ta):
@@ -197,97 +263,109 @@ def ZF_Factor(trace_list, cjmp_ta):
 #         ta -= 1
 #     return factors
 
-#
-# def CX_Factor(trace_list, cjmp_ta):
-#     factors = []
-#     ta = cjmp_ta - 1
-#     while True:
-#         if trace_list[ta].get_operand() in ARITHMETIC_OPERATORS:
-#             for reg in trace_list[ta].splitOperand():
-#                 if not reg.startswith("0x"):
-#                     factors.append(reg)
-#             break
-#         ta -= 1
-#     return ta
 
-
-def BackPropagation(trace_list, ta, max_ta, factor):
-    bp_factors = []
+# new
+def BackPropagation(trace_list, size, ta, max_ta, factors):
+    bp_factors = factors
     bp_ta = ta - 1
-    not_found_flag = True
+    influencing_ta_set = set()
+    influencing_ta_set.add(ta)
 
-    while bp_ta > max_ta or bp_ta >= 0:
-        regs = []
+    while bp_ta >= max_ta and bp_ta >= 0:
+        regs = trace_list[bp_ta].get_operand()
         if trace_list[bp_ta].get_name().upper() in OPERATORS:
-            regs = trace_list[bp_ta].get_operand()
-                #print("regs : ",regs)
-                # regs[0] == dst
-            if regs[0].upper() == factor:
-                not_found_flag = False
+            if RegisterTranslator(regs[0].upper()) in bp_factors:
+                influencing_ta_set.add(bp_ta)
                 for reg in regs:
                     if not reg.startswith("0x"):
-                            #print("bp_ta : ", bp_ta)
-                        bp_factors.append(reg.upper())
-                        #print("bp_fac : ", bp_factors)
-                break
+                        bp_factors.append(RegisterTranslator(reg.upper()))
         elif trace_list[bp_ta].get_name().upper() in DATA_MOVEMENT:
-            regs = trace_list[bp_ta].get_operand()
-            if regs[0].upper() == factor:
-                not_found_flag = False
-                if regs[1].startswith("0x"):
-                    bp_factors = []
-                else:
-                    bp_factors.append(regs[1].upper())
-                break
+            if RegisterTranslator(regs[0].upper()) in bp_factors:
+                influencing_ta_set.add(bp_ta)
+                bp_factors.remove(RegisterTranslator(regs[0].upper()))
+                if not regs[1].startswith("0x"):
+                    bp_factors.append(RegisterTranslator(regs[1].upper()))
+
+        if len(bp_factors) == 0:
+            break
+        if len(influencing_ta_set) == size:
+            break
         bp_ta -= 1
 
-    if not_found_flag:
-        bp_ta = ta
-    bp_factors = list(set(bp_factors))
-    return bp_factors, bp_ta
+        return influencing_ta_set
 
+
+# new
+def Recursive_BP(trace_list, factor_ta, max_ta, factors):
+    influencing_ta_set = BackPropagation(trace_list, 10, factor_ta, max_ta, factors)
+
+    return influencing_ta_set
+
+#  original
+# def BackPropagation(trace_list, cnt, ta, max_ta, factor, ta_min):
+#     bp_factors = []
+#     bp_ta = ta - 1
+#     ta_min_list = []
+#
+#     if cnt:
+#         while bp_ta >= max_ta and bp_ta >= 0:
+#             if trace_list[bp_ta].get_name().upper() in OPERATORS:
+#                 regs = trace_list[bp_ta].get_operand()
+#                 if RegisterTranslator(regs[0].upper()) == factor:
+#                     for reg in regs:
+#                         if not reg.startswith("0x"):
+#                             bp_factors.append(RegisterTranslator(reg.upper()))
+#                     break
+#             elif trace_list[bp_ta].get_name().upper() in DATA_MOVEMENT:
+#                 regs = trace_list[bp_ta].get_operand()
+#                 if RegisterTranslator(regs[0].upper()) == factor:
+#                     if regs[1].startswith("0x"):
+#                         bp_factors = []
+#                     else:
+#                         bp_factors.append(RegisterTranslator(regs[1].upper()))
+#                     break
+#             bp_ta -= 1
+#         if len(bp_factors) == 0:
+#             return ta_min
+#         elif len(bp_factors) == 1:
+#             return BackPropagation(trace_list, cnt-1, bp_ta, max_ta, bp_factors[0], bp_ta)
+#         else:
+#             for bp_factor in bp_factors:
+#                 ta_min_list.append(BackPropagation(trace_list, cnt-1, bp_ta, max_ta, bp_factor, ta_min))
+#             return min(ta_min_list)
+#     else:
+#         return ta_min
+#
+#
+# def Recursive_BP(trace_list, factor_ta, max_ta, factors):
+#     ta_min = factor_ta
+#     for factor in factors:
+#         ta_min = min(ta_min, BackPropagation(trace_list, 3, factor_ta, max_ta, factor, ta_min))
+#
+#     return ta_min
 
 def FigureK(trace_list, cjmp_ta):
-    cnt = 3
     factors = []
-    factor_ta_dict = dict()
     cjmp = trace_list[cjmp_ta].get_name().upper()
-    ta = cjmp_ta
+    factor_ta = cjmp_ta
     if cjmp in OF_JUMPS :
-        factors, ta = OF_Factor(trace_list, cjmp_ta)
+        factors, factor_ta = OF_Factor(trace_list, cjmp_ta)
     elif cjmp in SF_JUMPS:
-        factors, ta = SF_Factor(trace_list, cjmp_ta)
+        factors, factor_ta = SF_Factor(trace_list, cjmp_ta)
     elif cjmp in CF_JUMPS:
-        factors, ta = CF_Factor(trace_list, cjmp_ta)
+        factors, factor_ta = CF_Factor(trace_list, cjmp_ta)
     elif cjmp in ZF_JUMPS:
-        factors, ta = ZF_Factor(trace_list, cjmp_ta)
+        factors, factor_ta = ZF_Factor(trace_list, cjmp_ta)
     elif cjmp in CX_JUMPS:
-        factors = ["ECX"]
-        ################### implemnet here
+        factors, factor_ta = CX_Factor(trace_list, cjmp_ta)
 
-    for factor in factors:
-        factor_ta_dict[factor] = ta
-
-    print("factors : ", factors)
-
-    while cnt:
-        new_factors = []
-        for factor in factors:
-
-            bp_factors, bp_ta = BackPropagation(trace_list, factor_ta_dict[factor], cjmp_ta-30, factor)
-            for bp_factor in bp_factors:
-                factor_ta_dict[bp_factor] = bp_ta
-            new_factors.extend(bp_factors)
-
-        factors = list(set(new_factors))
-        print("update factors : ", 4-cnt, " // ", factors)
-        cnt -= 1
-
-    if factor_ta_dict is None:
-        ta_min = min(factor_ta_dict.values())
+    influencing_instruction_ta_list = Recursive_BP(trace_list, factor_ta, cjmp_ta-30,  factors)
+    if not influencing_instruction_ta_list is None:
+        ta_min = min(influencing_instruction_ta_list)
     else:
-        ta_min = cjmp_ta - 10
-    #print("cjump :", cjmp_ta, "ta_min : ", ta_min)
+        ta_min = cjmp_ta-10
+    print(influencing_instruction_ta_list)
+    print("ta_min : ", trace_list[ta_min].get_ta(), " cjmp_ta : ", trace_list[cjmp_ta].get_ta())
 
     return ta_min
 
@@ -332,20 +410,6 @@ def GetSymbVal(asmblocks, loc_db):
             sym_state.merge(symb.get_state())
     return sym_state, sym_pc
 
-
-# def GetKBlocks(trace_index, trace_list, asmblocks, k):
-#     asmblock_index = trace_list[trace_index].asmblock_index
-#     #set index
-#     index = asmblock_index
-#     start = 0
-#     end = index + 1
-#     if index < k:
-#         start = 0
-#     else:
-#         start = end - k
-#     kblocks = asmblocks[start: end]
-#
-#     return kblocks
 
 def IsUnsatTa(asmblocks, trace_index, trace_list, loc_db):
     try:
@@ -401,3 +465,5 @@ for trace_index in cjmp_index_list:
     # print(bin_str, " @ ", k," @ ", trace_index)
     asm_blocks, loc_db = ConstructAsmblock(bin_str)
     IsUnsatTa(asm_blocks, trace_index, trace_list, loc_db)
+
+
